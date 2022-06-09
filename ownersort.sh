7457e077b9190af1da361090d8079a01615400d1
Pcade <grishamindme@gmail.com>
2022-06-09 18:19:57 +0300

    delete local names from ownersort

--- ownersort.sh
+++ ownersort.sh
@@ -3,8 +3,8 @@
 #Создать скрипт ownersort.sh, который в заданной папке копирует файлы в директории, названные по имени владельца каждого файла.
 #Учтите, что файл должен принадлежать соответствующему владельцу. 
 
-cd /home && ls > /home/pcade/PycharmProjects/geek-python/Unix/lesson4/task4 # смотрю всех user через ls в home и передаю полученный результат в файл
-users=/home/pcade/PycharmProjects/geek-python/Unix/lesson4/task4 # присваиваю файлу переменную для удабства использования 
+cd /home && ls > /home/exampleWAY/filename # смотрю всех user через ls в home и передаю полученный результат в файл
+users=/home/exampleWAY/filename # присваиваю файлу переменную для удабства использования 
 cd -  # спускаюсь в начальную дирректорию где будет происходить процесс
 total=$(cat $users | wc -l) # подсчитываю колличество user
 counter=0
